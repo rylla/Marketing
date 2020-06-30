b44c00731f780106c4502bbd1967bc51a48fd82d
Afolabi Marvelous <afosmarvel@gmail.com>
2020-06-29 18:06:44 -0700

    Completed work

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('.hamburger').on('click', function() {
+        if($('.navs').hasClass('items')) {
+            $('.navs').removeClass('items')
+        }
+        else{
+            $('.navs').addClass('items')
+        }
+    })
+})
+
+
