3b87559d11bb9a33e0962803dc963dc08d986a2c
Afolabi Marvelous <afosmarvel@gmail.com>
2020-07-03 16:59:37 -0700

    made changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,29 @@
 $(function() {
-    $('.hamburger').on('click', function() {
-        if($('.navs').hasClass('items')) {
-            $('.navs').removeClass('items')
-        }
-        else{
-            $('.navs').addClass('items')
-        }
+    $('.hamburger').click(function() {
+        $(".header div nav ul").animate({
+            opacity: 1,
+            height: "toggle"
+        }, 300, function() {
+            $(".header div nav ul").toggleClass("items") ;
+        });
+    });
+
+    $('.li-option').click(function() {
+        $('ul').css('display', 'none')
     })
-})
+}) ;
 
 
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if(prevScrollpos > currentScrollPos) {
+        document.getElementById('header').style.top = "0"
+    }
+
+    else{
+        document.getElementById('header').style.top = "-90px"
+    }
+
+    prevScrollpos = currentScrollPos;
+}
